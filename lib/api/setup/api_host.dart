062a9876f29aeef8fac685595ffcf3dab9190229
rodrigogaleano <rodrigogaleano@jera.com.br>
2023-08-29 16:51:41 -0400

    [FIX] Fix api url

--- lib/api/setup/api_host.dart
+++ lib/api/setup/api_host.dart
@@ -2,7 +2,7 @@ class ApiHost {
   ApiHost._();
 
   static String get baseURL {
-    return 'https://pokeapi.co/api/$apiVersion';
+    return 'https://pokeapi.co/api$apiVersion';
   }
 
   static String get apiVersion => '/v2';
